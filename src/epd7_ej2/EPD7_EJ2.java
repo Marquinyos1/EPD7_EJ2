fa223b18d547a9234a871f40edd1a7a51f9131f5
mark3 <mark3@192.168.1.18>
2023-11-26 19:26:36 +0100

    Carga Inicial

---
+++ src/epd7_ej2/EPD7_EJ2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package epd7_ej2;
+
+/**
+ *
+ * @author mark3
+ */
+public class EPD7_EJ2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
